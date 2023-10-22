158569088769df032fdd0d1cfbf5341da68df41d
unknown <naazfalak5119@gmail.com>
2023-10-22 15:38:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import AgeCalculator from "./components/AgeCalculator";
+import "../src/components/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
